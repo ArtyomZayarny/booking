4d20f111b0e5d3a980e9ea038a2332e7a03e7ce6
ArtemZayarny <artyom.zayarny@gmail.com>
2024-02-02 10:18:35 +0200

    Added validation and logger

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  //White list ignore property thats doesn't exist in model
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
