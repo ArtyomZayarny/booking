e07c3cb39830e0ab6f312fa0a4ba45be2051d656
ArtemZayarny <artyom.zayarny@gmail.com>
2024-02-04 12:01:51 +0200

    local-strategy

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { UserDocument } from './users/models/user.schema';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+  async login(user: UserDocument, response: Response) {
+    //Create token payload
+    const tokenPayload = {
+      userId: user._id.toHexString(),
+    };
+
+    //set exp date for token
+    const expires = new Date();
+    expires.setSeconds(
+      expires.getSeconds() + this.configService.get('JWT_EXPIRATION'),
+    );
+
+    // Generate token
+    const token = this.jwtService.sign(tokenPayload);
+
+    //Set token to cookie
+    response.cookie('Authorization', token, {
+      httpOnly: true,
+      expires,
+    });
   }
 }
